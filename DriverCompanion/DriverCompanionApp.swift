e557fd668bba00f7fe514d6c5638179a43da95a4
DerikMalcolm <DerikMalcolm@gmail.com>
2022-09-24 01:27:05 -0400

    Initial Commit

---
+++ DriverCompanion/DriverCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriverCompanionApp.swift
+//  DriverCompanion
+//
+//  Created by Derik Malcolm on 9/24/22.
+//
+
+import SwiftUI
+
+@main
+struct DriverCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
